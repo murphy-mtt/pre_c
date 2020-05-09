d8ed256a69039c99d8d015786c186c59b707c24b
murphy-mtt <zd_wangfenglin@163.com>
2020-05-09 21:56:29 +0800

    test point %x

---
+++ point1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int var1;
+	char var2[10];
+
+	printf("var1 variable address: %p\n", &var1);
+	printf("var2 variable address: %p\n", &var2);
+
+	return 0;
+}
+
---
+++ point3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	int *ptr = NULL;
+	printf("ptr address is %p\n", ptr);
+	return 0;
+}
+
