39c84b3c901a9ec52e426686598f88db0f116250
murphy-mtt <zd_wangfenglin@163.com>
2020-05-15 21:49:55 +0800

    shell sort

---
+++ test_symbol.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 60;
+  int c;
+  int arr = { 22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70 };
+  c = a;
+  printf("%d\n", c);
+  c = a >> 2;
+  printf("%d\n", c);
+}
+
