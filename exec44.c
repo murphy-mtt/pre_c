84817901553971dda620c0db1e9128f3ee899992
murphy-mtt <zd_wangfenglin@163.com>
2020-07-02 20:58:32 +0800

    exec 47

---
+++ exec44.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int a, b, c;
+
+void add()
+{
+  int a;
+  a = 3;
+  c = a + b;
+}
+
+int main()
+{
+  a = b = 4;
+  add();
+  printf("c value is %d\n", c);
+  return 0;
+}
+
---
+++ exec45.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  register int i;
+  int tmp=0;
+  for (i=1; i<=100; i++)
+    tmp += i;
+  printf("Sum is %d\n", tmp);
+  return 0;
+}
+
